3ad7a649b5173da7f0b8ecd41b245b321956bda3
hachristinee <christinehl@google.com>
2022-08-22 16:52:39 +1000

    initial push

---
+++ database.js
@@ -0,0 +1,33 @@
+const Pool = require('pg').Pool
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASS,
+  port: 5432
+})
+
+const getProducts = (request, response) => {
+  pool.query('SELECT * FROM PRODUCTS ORDER BY id ASC', (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
+const createProduct = (request, response) => {
+  const { id, name, desc, size, price, date } = request.body
+  pool.query('INSERT INTO products VALUES ($1, $2, $3, $4, $5, $6) RETURNING *', [id, name, desc, size, price, date], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(201).send(`Product added with ID: ${results.rows[0].id}`)
+  })
+}
+
+module.exports = {
+  getProducts,
+  createProduct
+}
---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const db = require('./database')
+const bodyParser = require('body-parser')
+const app = express();
+const port = 3000;
+
+
+app.use(bodyParser.json())
+app.use(
+    bodyParser.urlencoded({
+      extended: true,
+    })
+)
+
+app.get('/', (request, response) => {
+  response.json({ info: 'Node.js, Express, and Postgres API' })
+})
+
+app.get('/products', db.getProducts)
+app.post('/createproduct', db.createProduct)
+
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}.`)
+})
